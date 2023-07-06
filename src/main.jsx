592e0e8cfe06f3af64055a4a8c3826698f7bf4fa
35kartal35 <mozdemir3545@gmail.com>
2023-07-06 20:20:23 +0300

    7. commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import verideposu from './redux/store.js'
+import veriler from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={verideposu}>
+    <Provider store={veriler}>
     <App />
     </Provider>
   </React.StrictMode>,
